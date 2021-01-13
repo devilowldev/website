301017940ec43848e2a6bcd99e79db6f9bcc290f
repl.it user <replituser@example.com>
2021-01-13 11:40:52 +0000

    Update

--- util/webhook.js
+++ util/webhook.js
@@ -1,11 +1,16 @@
 const { Webhook, MessageBuilder } = require('discord-webhook-node')
 const hook = new Webhook(process.env.WEBHOOK)
+const db = require('quick.db')
 
 module.exports = () => {
+  const x = db.get('restart')
+
     const embed = new MessageBuilder()
         .setTitle('Website')
         .setURL('https://www.null.com')
-        .setDescription(`Website Restart`)
+        .setDescription(`Website Restart (${x})`)
         .setTimestamp()
     hook.send(embed)
+
+    db.add('restart', 1)
 }
