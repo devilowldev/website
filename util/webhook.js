a9805f798bed6e9328084d6a48ec037af5a3fe47
1px <69074375+1GPEX@users.noreply.github.com>
2021-01-13 18:04:42 +0700

    Update webhook.js

--- util/webhook.js
+++ util/webhook.js
@@ -1,17 +1,11 @@
 const { Webhook, MessageBuilder } = require('discord-webhook-node')
 const hook = new Webhook(process.env.WEBHOOK)
-const { Database } = require('instant.db')
-const db = new Database()
 
 module.exports = () => {
-    const x = db.get('restart')
-
     const embed = new MessageBuilder()
         .setTitle('Website')
         .setURL('https://www.null.com')
-        .setDescription(`Website Restart (${x})`)
+        .setDescription(`Website Restart`)
         .setTimestamp()
     hook.send(embed)
-
-    db.add('restart', 1)
 }
