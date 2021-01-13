8ed1a70537ba2586d21c94f55c5c7f548661209f
1GPEX <1GPEX>
2021-01-13 18:01:46 +0700

    New Webhook

--- util/webhook.js
+++ util/webhook.js
@@ -1,13 +1,17 @@
 const { Webhook, MessageBuilder } = require('discord-webhook-node')
 const hook = new Webhook(process.env.WEBHOOK)
-const db = require('quick.db')
+const { Database } = require('instant.db')
+const db = new Database()
 
 module.exports = () => {
+    const x = db.get('restart')
+
     const embed = new MessageBuilder()
         .setTitle('Website')
         .setURL('https://www.null.com')
-        .setDescription(`Website Restart(${db.get('restart')})`)
+        .setDescription(`Website Restart (${x})`)
         .setTimestamp()
     hook.send(embed)
+
     db.add('restart', 1)
 }
